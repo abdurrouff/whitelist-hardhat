4c262cf0b64307677a31a5679c1434164cc2675a
Rawman <rouffaderemi@gmail.com>
2023-06-12 19:38:38 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config({ path: '.env' });
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: '0.8.9',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
